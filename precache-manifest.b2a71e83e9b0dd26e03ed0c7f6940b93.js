99bd3d19ffe4208289bff1f8115a2968a7a4de55
Alc499 <Ali.vitchenko@gmail.com>
2019-08-06 14:39:30 +0300

    Updates

---
+++ precache-manifest.b2a71e83e9b0dd26e03ed0c7f6940b93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4936eb78c641cb32910cf2350d7aa55a",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "5338b1e4d1a7b9800fce",
+    "url": "/react_pagination/static/css/main.0e698936.chunk.css"
+  },
+  {
+    "revision": "70b015041aee776b843e",
+    "url": "/react_pagination/static/js/2.70a02d18.chunk.js"
+  },
+  {
+    "revision": "5338b1e4d1a7b9800fce",
+    "url": "/react_pagination/static/js/main.d2f867f6.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
